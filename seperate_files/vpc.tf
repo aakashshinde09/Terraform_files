27f01cc2227233b4e123e579963318884c7ae67a
Akash Shinde <saakashs1999@gmail.com>
2024-05-30 18:53:20 +0530

    updated

--- seperate_files/vpc.tf
+++ seperate_files/vpc.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "terraformstatefile.tf"
+    bucket = "akash-terraform"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
